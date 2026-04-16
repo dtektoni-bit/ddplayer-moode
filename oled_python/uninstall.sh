eb948a46f132e0d442d9a3e8eedd811154b90fc0
dtektoni-bit <dtektoni@gmail.com>
2026-04-16 12:09:42 +0300

    Add files via upload

---
+++ oled_python/uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "=== DDPlayer OLED uninstall ==="
+
+sudo systemctl stop ddplayer-oled
+sudo systemctl disable ddplayer-oled
+sudo rm -f /etc/systemd/system/ddplayer-oled.service
+sudo systemctl daemon-reload
+
+rm -f /home/moode/ddplayer_oled.py
+rm -f /home/moode/setup_oled.py
+rm -f /home/moode/adafruit_font.py
+rm -f /home/moode/make_font.py
+
+sudo rm -f /usr/share/fonts/truetype/Monocraft.ttf
+sudo fc-cache -f
+
+echo "=== uninstalled ==="
